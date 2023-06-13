8d1a6d62e672ca2696a52e01c83e81be00028711
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2023-06-13 12:40:46 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+void Update ()
+ {
+     if (Input.GetKey(KeyCode.LeftArrow))
+     {
+         transform.position += Vector3.left * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.RightArrow))
+     {
+         transform.position += Vector3.right * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.UpArrow))
+     {
+         transform.position += Vector3.up * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.DownArrow))
+     {
+         transform.position += Vector3.down * speed * Time.deltaTime;
+     }
+ }
